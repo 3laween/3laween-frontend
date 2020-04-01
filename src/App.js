6acf15994d6a21ec6ba07a7830042a9bc3228383
Mahmoud <info@3laween.com>
2020-04-01 06:35:42 +0400

    first frontend deploy

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import Note from './components/Note'
+
+
+const App = (props) => {
+    const [notes, setNotes] = useState([])
+
+    const eventHandler = response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      }
+
+    const hook = () =>{
+        axios
+          .get('/api/notes')
+          .then(eventHandler)
+    } 
+    
+    useEffect(hook,[])
+      console.log('render', notes.length, 'notes')
+
+    const addNote = (event) => {
+        event.preventDefault()
+        console.log('button clicked', event.target)
+        const noteObject = {
+            content: newNote,
+            date: new Date().toISOString(),
+            important: Math.random() > 0.5,
+            id: notes.length + 1,
+          }
+        axios.post("/api/notes",noteObject).then(response => {
+            setNotes(notes.concat(response.data))
+            setNewNotes('')
+        })
+    }
+
+    const [newNote, setNewNotes] = useState(
+        'a new note...'
+    )
+
+    const handleNoteChange = (event) => {
+        setNewNotes(event.target.value)
+    }
+
+    return (
+      <div>
+        <h1>Notes</h1>
+        <ul>
+         {notes.map((note,i) => 
+         <Note key={i} note={note} />
+         )}
+        </ul>
+
+        <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+
+      </div>
+    )
+}
+
+export default App
